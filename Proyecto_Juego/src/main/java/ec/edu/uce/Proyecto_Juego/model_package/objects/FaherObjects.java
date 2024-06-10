cb80c16bd18f737c48e7dd412fc1145c3a556d56
Narban62 <narban62@gmail.com>
2024-06-10 02:29:16 -0500

    Final

---
+++ Proyecto_Juego/src/main/java/ec/edu/uce/Proyecto_Juego/model_package/objects/FaherObjects.java
@@ -0,0 +1,32 @@
+package ec.edu.uce.Proyecto_Juego.model_package.objects;
+
+public class FaherObjects {
+
+    int[] pointsX;
+    int[] pointsY;
+    int life;
+
+    public int[] getPointsX() {
+        return pointsX;
+    }
+
+    public void setPointsX(int[] pointsX) {
+        this.pointsX = pointsX;
+    }
+
+    public int[] getPointsY() {
+        return pointsY;
+    }
+
+    public void setPointsY(int[] pointsY) {
+        this.pointsY = pointsY;
+    }
+
+    public int getLife() {
+        return life;
+    }
+
+    public void setLife(int life) {
+        this.life = life;
+    }
+}
